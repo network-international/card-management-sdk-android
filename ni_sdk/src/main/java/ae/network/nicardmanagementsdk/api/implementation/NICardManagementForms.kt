01f2d8d1a04083c89bab6fb5b6f7f796276506dc
Ciprian Salomir <ciprian.salomir@gmail.com>
2023-01-17 15:46:39 +0200

    SDK Remove old SetPin and ChangePin API

--- ni_sdk/src/main/java/ae/network/nicardmanagementsdk/api/implementation/NICardManagementForms.kt
+++ ni_sdk/src/main/java/ae/network/nicardmanagementsdk/api/implementation/NICardManagementForms.kt
@@ -2,14 +2,11 @@ package ae.network.nicardmanagementsdk.api.implementation
 
 import ae.network.nicardmanagementsdk.api.interfaces.NICardManagementFormsAPI
 import ae.network.nicardmanagementsdk.api.models.input.NIInput
-import ae.network.nicardmanagementsdk.api.models.input.NIPinFormType
 import ae.network.nicardmanagementsdk.api.models.output.NIErrorResponse
 import ae.network.nicardmanagementsdk.api.models.output.NISuccessResponse
 import ae.network.nicardmanagementsdk.presentation.extension_methods.getSerializableExtraCompat
 import ae.network.nicardmanagementsdk.presentation.models.Extra
 import ae.network.nicardmanagementsdk.presentation.ui.card_details.CardDetailsActivity
-import ae.network.nicardmanagementsdk.presentation.ui.change_pin.ChangePinActivity
-import ae.network.nicardmanagementsdk.presentation.ui.set_pin.SetPinActivity
 import android.app.Activity
 import android.content.Intent
 import androidx.activity.ComponentActivity
@@ -21,27 +18,15 @@ typealias OnSuccessErrorCancelCompletion = (isSuccess: NISuccessResponse?, isErr
 
 class NICardManagementForms(
     private val activity: ComponentActivity,
-    displayCardDetailsOnCompletion: OnSuccessErrorCancelCompletion = { _, _, _ -> },
-    setPinOnCompletion: OnSuccessErrorCancelCompletion = { _, _, _ -> },
-    changePinOnCompletion: OnSuccessErrorCancelCompletion = { _, _, _ -> },
+    displayCardDetailsOnCompletion: OnSuccessErrorCancelCompletion = { _, _, _ -> }
 ) : NICardManagementFormsAPI {
 
     private var displayCardResultLauncher: ActivityResultLauncher<Intent>? = null
-    private var setPinResultLauncher: ActivityResultLauncher<Intent>? = null
-    private var changePinResultLauncher: ActivityResultLauncher<Intent>? = null
 
     init {
 
         displayCardResultLauncher = activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
             onGetResultsFromActivity(activityResult, displayCardDetailsOnCompletion)
-        }
-
-        setPinResultLauncher = activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
-            onGetResultsFromActivity(activityResult, setPinOnCompletion)
-        }
-
-        changePinResultLauncher = activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
-            onGetResultsFromActivity(activityResult, changePinOnCompletion)
         }
     }
 
@@ -50,26 +35,6 @@ class NICardManagementForms(
     ) {
         displayCardResultLauncher?.launch(Intent(activity, CardDetailsActivity::class.java).apply {
             putExtra(Extra.EXTRA_NI_INPUT, input)
-        })
-    }
-
-    override fun setPinForm(
-        input: NIInput,
-        type: NIPinFormType
-    ) {
-        setPinResultLauncher?.launch(Intent(activity, SetPinActivity::class.java).apply {
-            putExtra(Extra.EXTRA_NI_INPUT, input)
-            putExtra(Extra.EXTRA_NI_PIN_FORM_TYPE, type)
-        })
-    }
-
-    override fun changePinForm(
-        input: NIInput,
-        type: NIPinFormType
-    ) {
-        changePinResultLauncher?.launch(Intent(activity, ChangePinActivity::class.java).apply {
-            putExtra(Extra.EXTRA_NI_INPUT, input)
-            putExtra(Extra.EXTRA_NI_PIN_FORM_TYPE, type)
         })
     }
 
--- ni_sdk/src/main/java/ae/network/nicardmanagementsdk/api/interfaces/NICardManagementFormsAPI.kt
+++ ni_sdk/src/main/java/ae/network/nicardmanagementsdk/api/interfaces/NICardManagementFormsAPI.kt
@@ -1,21 +1,10 @@
 package ae.network.nicardmanagementsdk.api.interfaces
 
 import ae.network.nicardmanagementsdk.api.models.input.NIInput
-import ae.network.nicardmanagementsdk.api.models.input.NIPinFormType
 
 interface NICardManagementFormsAPI {
 
     fun displayCardDetailsForm(
         input: NIInput
-    )
-
-    fun setPinForm(
-        input: NIInput,
-        type: NIPinFormType = NIPinFormType.DYNAMIC
-    )
-
-    fun changePinForm(
-        input: NIInput,
-        type: NIPinFormType = NIPinFormType.DYNAMIC
     )
 }
