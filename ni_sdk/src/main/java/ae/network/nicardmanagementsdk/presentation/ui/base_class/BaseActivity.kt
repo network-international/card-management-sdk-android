38166ed2e230df1721ae7ea2cc9343a4774c5936
Ciprian Salomir <ciprian.salomir@gmail.com>
2023-06-07 15:26:48 +0300

    Change theme attribute to the initial meaning with Day/Night mode support (LIGHT, DARK_APP_COMPAT, DARK_MATERIAL)

--- ni_sdk/src/main/java/ae/network/nicardmanagementsdk/presentation/ui/base_class/BaseActivity.kt
+++ ni_sdk/src/main/java/ae/network/nicardmanagementsdk/presentation/ui/base_class/BaseActivity.kt
@@ -1,6 +1,7 @@
 package ae.network.nicardmanagementsdk.presentation.ui.base_class
 
 import ae.network.nicardmanagementsdk.api.models.input.NIInput
+import ae.network.nicardmanagementsdk.helpers.ThemeHelper
 import ae.network.nicardmanagementsdk.presentation.extension_methods.getSerializableExtraCompat
 import ae.network.nicardmanagementsdk.presentation.models.Extra
 import android.app.Activity
@@ -20,6 +21,7 @@ abstract class BaseActivity<T: BaseViewModel> : AppCompatActivity() {
             niInput = it
         } ?: throw RuntimeException("${this::class.java.simpleName} intent serializable ${Extra.EXTRA_NI_INPUT} is missing")
 
+        setTheme(ThemeHelper().getThemeResId(niInput))
     }
 
     protected fun navigateBack() {
