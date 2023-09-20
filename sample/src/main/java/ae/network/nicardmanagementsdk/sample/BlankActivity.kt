c73ce1968a35965e8a1feed66676ba21add7913e
Yousuf Sohail <yousef.sohail@tamara.co>
2023-09-20 14:42:43 +0500

    add sample app so that changes in SDK can be tested instantly wo publishing

---
+++ sample/src/main/java/ae/network/nicardmanagementsdk/sample/BlankActivity.kt
@@ -0,0 +1,12 @@
+package ae.network.nicardmanagementsdk.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nicardmanagementapp.R
+
+class BlankActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_blank)
+    }
+}
